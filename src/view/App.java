098d8cc263784e1c438ca274f9c7bb3b40a371ce
cristian66333 <74688238+Cristian66333@users.noreply.github.com>
2025-05-05 09:19:48 -0500

    final

--- src/view/App.java
+++ src/view/App.java
@@ -11,19 +11,23 @@ public class App {
     public static void main(String[] args) {
         Calculadora control = new Calculadora();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese numero");
-        int numero = sc.nextInt();
-        System.out.println("Ingrese orden");
-        int orden = sc.nextInt();
-        try {
+        while (true) {
+            try {
+                System.out.println("Ingrese numero");
+                int numero = sc.nextInt();
+                System.out.println("Ingrese orden");
+                int orden = sc.nextInt();
+                System.out.println(control.calcularRaiz(numero, orden));
+                System.out.println("Adios");
+                break;
+            } catch (NumeroImaginarioException eImaginario) {
+                System.out.println(eImaginario.getMessage());
+            } catch (ArithmeticException eArithmetic) {
+                System.out.println(eArithmetic.getMessage());
+            } catch (RaizImparException eImpar) {
+                System.out.println(eImpar.getMessage());
+            }
 
-            System.out.println(control.calcularRaiz(numero, orden));
-        } catch (NumeroImaginarioException eImaginario) {
-            System.out.println(eImaginario.getMessage());        
-        } catch (ArithmeticException eArithmetic){
-            System.out.println(eArithmetic.getMessage());
-        }catch (RaizImparException eImpar){
-            System.out.println(eImpar.getMessage());
         }
 
     }
